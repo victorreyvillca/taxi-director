5f15fe8c56943e4f7588214cd0c5183396d13478
Victor Villca <victor.villca@people-t.com>
2014-03-17 12:48:54 -0400

    TA-0000: Commeting the model object to save in the tables

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,70 +13,70 @@ class IndexController extends Dis_Controller_Action {
     }
 
     public function indexAction() {
-        $person = new Person();
-        $person
-            ->setSex(1)
-            ->setState(TRUE)
-            ->setCreated(new DateTime('now'))
-            ->setPhonework('fdsfds')
-            ->setPhonemobil(456465)
-            ->setPhone('fdafda')
-            ->setDateOfBirth(new DateTime('now'))
-            ->setIdentityCard(465465)
-            ->setLastName('fdsfds')
-            ->setFirstName('vvdf');
+//         $person = new Person();
+//         $person
+//             ->setSex(1)
+//             ->setState(TRUE)
+//             ->setCreated(new DateTime('now'))
+//             ->setPhonework('fdsfds')
+//             ->setPhonemobil(456465)
+//             ->setPhone('fdafda')
+//             ->setDateOfBirth(new DateTime('now'))
+//             ->setIdentityCard(465465)
+//             ->setLastName('fdsfds')
+//             ->setFirstName('vvdf');
 
-        $this->_entityManager->persist($person);
-        $this->_entityManager->flush();
+//         $this->_entityManager->persist($person);
+//         $this->_entityManager->flush();
 
-        $passenger = new Passenger();
-        $passenger
-            ->setAddress('addres')
-            ->setDescription('fdafda')
-            ->setSex(1)
-            ->setState(TRUE)
-            ->setCreated(new DateTime('now'))
-            ->setPhonework('fdsfds')
-            ->setPhonemobil(456465)
-            ->setPhone('fdafda')
-            ->setDateOfBirth(new DateTime('now'))
-            ->setIdentityCard(465465)
-            ->setLastName('fdsfds')
-            ->setFirstName('vvdf');
+//         $passenger = new Passenger();
+//         $passenger
+//             ->setAddress('addres')
+//             ->setDescription('fdafda')
+//             ->setSex(1)
+//             ->setState(TRUE)
+//             ->setCreated(new DateTime('now'))
+//             ->setPhonework('fdsfds')
+//             ->setPhonemobil(456465)
+//             ->setPhone('fdafda')
+//             ->setDateOfBirth(new DateTime('now'))
+//             ->setIdentityCard(465465)
+//             ->setLastName('fdsfds')
+//             ->setFirstName('vvdf');
 
-        $this->_entityManager->persist($passenger);
-        $this->_entityManager->flush();
+//         $this->_entityManager->persist($passenger);
+//         $this->_entityManager->flush();
 
-        $driver = new Driver();
-        $driver
-        ->setAddress('addres')
-//         ->setDescription('fdafda')
-        ->setSex(1)
-        ->setState(TRUE)
-        ->setCreated(new DateTime('now'))
-        ->setPhonework('fdsfds')
-        ->setPhonemobil(456465)
-        ->setPhone('fdafda')
-        ->setDateOfBirth(new DateTime('now'))
-        ->setIdentityCard(465465)
-        ->setLastName('fdsfds')
-        ->setFirstName('vvdf');
+//         $driver = new Driver();
+//         $driver
+//         ->setAddress('addres')
+// //         ->setDescription('fdafda')
+//         ->setSex(1)
+//         ->setState(TRUE)
+//         ->setCreated(new DateTime('now'))
+//         ->setPhonework('fdsfds')
+//         ->setPhonemobil(456465)
+//         ->setPhone('fdafda')
+//         ->setDateOfBirth(new DateTime('now'))
+//         ->setIdentityCard(465465)
+//         ->setLastName('fdsfds')
+//         ->setFirstName('vvdf');
 
-        $this->_entityManager->persist($driver);
-        $this->_entityManager->flush();
+//         $this->_entityManager->persist($driver);
+//         $this->_entityManager->flush();
 
-        $role = new Role();
-        $role->setName('dadda')->setDescription('dad')->setState(TRUE)->setCreated(new DateTime('now'));
-        $this->_entityManager->persist($role);
-        $this->_entityManager->flush();
+//         $role = new Role();
+//         $role->setName('dadda')->setDescription('dad')->setState(TRUE)->setCreated(new DateTime('now'));
+//         $this->_entityManager->persist($role);
+//         $this->_entityManager->flush();
 
-        $account = new Account();
-        $account
-        ->setAccountType(1)
-        ->setUsername('vvf')->setPassword('dfadf')->setEmail('dada')->setRole('dsad')->setState(TRUE)->setCreated(new DateTime('now'));
+//         $account = new Account();
+//         $account
+//         ->setAccountType(1)
+//         ->setUsername('vvf')->setPassword('dfadf')->setEmail('dada')->setRole('dsad')->setState(TRUE)->setCreated(new DateTime('now'));
 
-        $this->_entityManager->persist($account);
-        $this->_entityManager->flush();
+//         $this->_entityManager->persist($account);
+//         $this->_entityManager->flush();
 
     }
 }
