eb0d21b0f924e6441778fb4dad8dc26acb50b33b
Victor Villca <victor.villca@people-t.com>
2014-03-21 16:05:47 -0400

    TA-0010: Creating structure JSON-RPC client and server php

---
+++ application/controllers/ClientController.php
@@ -0,0 +1,33 @@
+<?php
+
+class ClientController extends Zend_Controller_Action {
+
+    public function init() {
+    	/* Initialize action controller here */
+    }
+
+    public function indexAction() {
+        $client = new Zend_XmlRpc_Client('http://localhost/taxi-director/public/server');
+
+        try {
+//         	$data = $client->call('cf.test');
+        	$data = $client->call('cf.getData', 5);
+        	$this->view->data = $data;
+        	$httpClient = $client->getHttpClient();
+        	$response = $httpClient->getLastResponse();
+        	echo(strlen($response->getRawBody())/1024);
+        } catch (Zend_XmlRpc_Client_HttpException $e) {
+        	require_once 'Zend/Exception.php';
+        	throw new Zend_Exception($e);
+        } catch (Zend_XmlRpc_Client_FaultException $e) {
+        	require_once 'Zend/Exception.php';
+        	throw new Zend_Exception($e);
+        }
+    }
+
+    public function jsonAction()
+    {
+    	$this->view->serverUrl = 'http://localhost/taxi-director/public/server/json';
+    	$this->view->dataNumber = 200;
+    }
+}
