468f0658b61c305b18a0eb4d7c5ed77b0b3a319b
Victor Villca Villca <victor.villca.v@gmail.com>
2014-04-22 05:43:50 -0430

    TA-0011: Implementing methods in the MovilController

--- application/Model/Repositories/TaxiRepository.php
+++ application/Model/Repositories/TaxiRepository.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Model\Repositories;
-
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -14,12 +12,14 @@ class TaxiRepository extends EntityRepository {
 
     /**
      * Alias of the table
+     *
      * @var string
      */
     private $_alias = 'taxi';
 
     /**
      * Returns models according the filters
+     *
      * @param array $filters
      * @param int $limit
      * @param int $offset
@@ -27,189 +27,277 @@ class TaxiRepository extends EntityRepository {
      * @param string $sortDirection
      * @return Array Objects
      */
-    public function findByCriteria($filters = array(), $limit = NULL, $offset = NULL, $sortColumn = NULL, $sortDirection = NULL) {
-    	$query = $this->_em->createQueryBuilder();
+    public function findByCriteria ($filters = array(), $limit = NULL, $offset = NULL,
+            $sortColumn = NULL, $sortDirection = NULL) {
+        $query = $this->_em->createQueryBuilder();
 
-    	$condName = "";
-    	foreach ($filters as $filter) {
-    		$condName = "$this->_alias.status = :status AND ";
-    		$query->setParameter($filter['field'], $filter['filter']);
-    	}
+        $condName = "";
+        foreach ($filters as $filter) {
+            $condName = "$this->_alias.status = :status AND ";
+            $query->setParameter($filter['field'], $filter['filter']);
+        }
 
-    	$query->select($this->_alias)
-    	->from($this->_entityName, $this->_alias)
-    	->where("$condName $this->_alias.state = TRUE")
-    	->setFirstResult($offset)
-    	->setMaxResults($limit)
-    	;
+        $query->select($this->_alias)
+            ->from($this->_entityName, $this->_alias)
+            ->where("$condName $this->_alias.state = TRUE")
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
 
-    	$sort = '';
-    	switch ($sortColumn) {
-    		case 1:
-    			$sort = 'name';
-    			break;
+        $sort = '';
+        switch ($sortColumn) {
+            case 1:
+                $sort = 'name';
+                break;
 
-    		case 2:
-    			$sort = 'description';
-    			break;
+            case 2:
+                $sort = 'description';
+                break;
 
-    		case 3:
-    			$sort = 'created';
-    			break;
+            case 3:
+                $sort = 'created';
+                break;
 
-    		case 4:
-    			$sort = 'changed';
-    			break;
+            case 4:
+                $sort = 'changed';
+                break;
 
-    		default: $sort = 'id'; $sortDirection = 'desc';
-    	}
+            default:
+                $sort = 'id';
+                $sortDirection = 'desc';
+        }
 
-    	$query->orderBy("$this->_alias.$sort", $sortDirection);
+        $query->orderBy("$this->_alias.$sort", $sortDirection);
 
-    	return $query->getQuery()->getResult();
+        return $query->getQuery()->getResult();
     }
 
     /**
      * Finds total count of models according the filters
+     *
      * @param array $filters
      * @return int
      */
-    public function getTotalCount($filters = array()) {
-    	$query = $this->_em->createQueryBuilder();
+    public function getTotalCount ($filters = array()) {
+        $query = $this->_em->createQueryBuilder();
 
-    	$condName = "";
-    	foreach ($filters as $filter) {
-    		$condName = "$this->_alias.name LIKE :name AND ";
-    		$query->setParameter($filter['field'], $filter['filter']);
-    	}
+        $condName = "";
+        foreach ($filters as $filter) {
+            $condName = "$this->_alias.name LIKE :name AND ";
+            $query->setParameter($filter['field'], $filter['filter']);
+        }
 
-    	$query->select("count($this->_alias.id)")
-    	->from($this->_entityName, $this->_alias)
-    	->where("$condName $this->_alias.state = TRUE");
+        $query->select("count($this->_alias.id)")
+            ->from($this->_entityName, $this->_alias)
+            ->where("$condName $this->_alias.state = TRUE");
 
-    	return (int)$query->getQuery()->getSingleScalarResult();
+        return (int) $query->getQuery()->getSingleScalarResult();
     }
 
     /**
      * (non-PHPdoc)
+     *
      * @see Doctrine\ORM.EntityRepository::findAll()
      */
-    public function findAll() {
-    	return $this->findBy(array('state' => TRUE));
+    public function findAll () {
+        return $this->findBy(array('state' => TRUE));
     }
 
     /**
      * Returns all club pathfinders
+     *
      * @return array
      */
-    public function findAllArray() {
-    	$items = $this->findAll();
+    public function findAllArray () {
+        $items = $this->findAll();
 
-    	$itemArray = array();
-    	foreach ($items as $item) {
-    		$itemArray[$item->getId()] = $item->getName();
-    	}
+        $itemArray = array();
+        foreach ($items as $item) {
+            $itemArray[$item->getId()] = $item->getName();
+        }
 
-    	return $itemArray;
+        return $itemArray;
     }
 
     /**
      * Returns the taxi models by status
+     *
      * @param int $status
      * @return array
      */
-    public function findByStatusArray($status) {
-    	$items = $this->findBy(array('state' => TRUE, 'status' => $status));
+    public function findByStatusArray ($status) {
+        $items = $this->findBy(array('state' => TRUE, 'status' => $status));
 
-    	$itemArray = array();
-    	foreach ($items as $item) {
-    		$itemArray[$item->getId()] = $item->getName();
-    	}
+        $itemArray = array();
+        foreach ($items as $item) {
+            $itemArray[$item->getId()] = $item->getName();
+        }
 
-    	return $itemArray;
+        return $itemArray;
     }
 
     /**
      * Returns the taxi models by status
+     *
      * @param int $status
      * @return array
      */
-    public function findByStatus($status) {
-    	return $this->findBy(array('state' => TRUE, 'status' => $status));
+    public function findByStatus ($status) {
+        return $this->findBy(array('state' => TRUE, 'status' => $status));
     }
 
     /**
      * Returns Taxi model by codeactivation
+     *
      * @param string $codeactivation
      * @return Taxi
      */
-    public function findByCodeactivation($codeactivation) {
-    	return $this->findOneBy(array('codeactivation' => $codeactivation, 'state' => TRUE));
+    public function findByCodeactivation ($codeactivation)
+    {
+        return $this->findOneBy(
+                array(
+                        'codeactivation' => $codeactivation,
+                        'state' => TRUE
+                ));
     }
 
     /**
      * Returns Taxi model by phone
+     *
      * @param string $phone
      * @return Taxi
      */
-    public function findByPhone($phone) {
-    	return $this->findOneBy(array('phone' => $phone, 'state' => TRUE));;
+    public function findByPhone ($phone)
+    {
+        return $this->findOneBy(
+                array(
+                        'phone' => $phone,
+                        'state' => TRUE
+                ));
+        ;
+    }
+
+    /**
+     * Returns Taxi model by phone and codeativation.
+     *
+     * @param string $phone
+     * @param string $codeactivation
+     * @return Taxi
+     */
+    public function findByPhoneCodigoActivacion ($phone, $codeactivation)
+    {
+        return $this->findOneBy(
+                array(
+                        'active' => TRUE,
+                        'phone' => $phone,
+                        'codeactivation' => $codeactivation,
+                        'state' => TRUE
+                ));
+        ;
     }
 
     /**
      * Returns the taxi models by status
+     *
      * @param int $status
      * @return array
      */
-    public function findByStatusArrayNumber($status) {
-    	$items = $this->findBy(array('state' => TRUE, 'status' => $status));
+    public function findByStatusArrayNumber ($status)
+    {
+        $items = $this->findBy(
+                array(
+                        'state' => TRUE,
+                        'status' => $status
+                ));
 
-    	$itemArray = array();
-    	$itemArray[-1] = _('--Seleccione--');
-    	foreach ($items as $item) {
-    		$itemArray[$item->getId()] = $item->getNumber();
-    	}
+        $itemArray = array();
+        $itemArray[- 1] = _('--Seleccione--');
+        foreach ($items as $item) {
+            $itemArray[$item->getId()] = $item->getNumber();
+        }
 
-    	return $itemArray;
+        return $itemArray;
     }
 
     /**
      * Verifies if the number already exist it.
+     *
      * @param int $number
      * @return boolean
      */
-    public function verifyExistNumber($number) {
-    	$object = $this->findOneBy(array('number' => $number, 'state' => TRUE));
-    	return $object != NULL? TRUE : FALSE;
+    public function verifyExistNumber ($number)
+    {
+        $object = $this->findOneBy(
+                array(
+                        'number' => $number,
+                        'state' => TRUE
+                ));
+        return $object != NULL ? TRUE : FALSE;
     }
 
     /**
      * Verifies if the id and number already exist it.
+     *
      * @param int $id
      * @param int $number
      */
-    public function verifyExistIdAndNumber($id, $number) {
-    	$object = $this->findOneBy(array('id' => $id, 'number' => $number, 'state' => TRUE));
-    	return $object != NULL? TRUE : FALSE;
+    public function verifyExistIdAndNumber ($id, $number)
+    {
+        $object = $this->findOneBy(
+                array(
+                        'id' => $id,
+                        'number' => $number,
+                        'state' => TRUE
+                ));
+        return $object != NULL ? TRUE : FALSE;
     }
 
     /**
      * Verifies if the phone already exist it.
+     *
      * @param string $phone
      * @return boolean
      */
-    public function verifyExistPhone($phone) {
-    	$object = $this->findOneBy(array('phone' => $phone, 'state' => TRUE));
-    	return $object != NULL? TRUE : FALSE;
+    public function verifyExistPhone ($phone)
+    {
+        $object = $this->findOneBy(
+                array(
+                        'phone' => $phone,
+                        'state' => TRUE
+                ));
+        return $object != NULL ? TRUE : FALSE;
     }
 
     /**
      * Verifies if the id and number already exist it.
+     *
      * @param int $id
      * @param string $phone
      */
-    public function verifyExistIdAndPhone($id, $phone) {
-    	$object = $this->findOneBy(array('id' => $id, 'phone' => $phone, 'state' => TRUE));
-    	return $object != NULL? TRUE : FALSE;
+    public function verifyExistIdAndPhone ($id, $phone)
+    {
+        $object = $this->findOneBy(
+                array(
+                        'id' => $id,
+                        'phone' => $phone,
+                        'state' => TRUE
+                ));
+        return $object != NULL ? TRUE : FALSE;
+    }
+
+    /**
+     * Verifies if the taxi are activation Taxi.
+     *
+     * @param string $codeactivation
+     * @param string $phone
+     */
+    public function verifyActivavionTaxi ($codeactivation, $phone) {
+        $object = $this->findOneBy(
+                array(
+                        'codeactivation' => $codeactivation,
+                        'phone' => $phone,
+                        'state' => TRUE,
+                        'active' => TRUE
+                ));
+        return $object != NULL ? TRUE : FALSE;
     }
 }
--- application/controllers/MovilController.php
+++ application/controllers/MovilController.php
@@ -1,4 +1,6 @@
 <?php
+use Model\Backtrack;
+
 /**
  * Controller for Taxi Director.
  *
@@ -7,115 +9,149 @@
  * @copyright Copyright (c) 2014 LeaderSoft A/S
  * @license Proprietary
  */
+class MovilController extends Dis_Controller_Action
+{
 
-class MovilController extends Dis_Controller_Action {
-
-    public function init() {
+    public function init ()
+    {
         /* Initialize action controller here */
     }
 
-    public function indexAction() {
+    public function indexAction ()
+    {
         if (isset($_POST['tag']) && $_POST['tag'] != '') {
-        	// get tag
-        	$tag = $_POST['tag'];
+            // get tag
+            $tag = $_POST['tag'];
 
-        	// response Array
-        	$response = array("success" => "OK");
+            // response Array
+            $response = array(
+                    "success" => ''
+            );
 
-        	switch ($tag) {
-        		case 'registrar':
-        			//*--- recepcion de datos
-        			$phone = $_POST['tel'];
-        			$taxiRepo = $this->_entityManager->getRepository('Model\Taxi');
+            switch ($tag) {
+                case 'registrar':
+                    // *--- recepcion de datos
+                    $phone = $_POST['tel'];
+                    $taxiRepo = $this->_entityManager->getRepository(
+                            'Model\Taxi');
                     if ($taxiRepo->verifyExistPhone($phone)) {
-//                         $codeactivation = "ccsadas12321";//self::generateKey();
-//                         $codeuser = "46512";//substr(str_shuffle(str_repeat('123456789', 2)), 0, 5);
-
-//                         $codeactivation = substr(str_shuffle(str_repeat('123456789', 2)), 0, 5);
-                        $codeuser = substr(str_shuffle(str_repeat('123456789', 2)), 0, 5);
-//                         $codeactivation = substr(str_shuffle(str_repeat('ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789', 4)), 0, 32);
+                        $codeuser = substr(
+                        str_shuffle(str_repeat('123456789', 2)), 0, 5);
                         $codeactivation = $this->generateKey();
 
-                        $response["success"] = "OK";//vacio en caso de errorad
+                        $response["success"] = "OK"; // vacio en caso de errorad
                         $response["codigoactivacion"] = $codeactivation;
                         $response["codigouser"] = $codeuser;
 
                         $taxi = $taxiRepo->findByPhone($phone);
 
-                        $taxi
-                            ->setCodeactivation($codeactivation)
-                            ->setCodeuser($codeuser)
-                        ;
+                        $taxi->setCodeactivation($codeactivation)->setCodeuser(
+                                $codeuser);
 
                         $this->_entityManager->persist($taxi);
                         $this->_entityManager->flush();
-        			} else {
-        			    $response["success"] = "";//vacio en caso de error
-        			}
-        			//*--- envio de datos
+                    } else {
+                        $response["success"] = ""; // vacio en caso de error
+                    }
+                    // *--- envio de datos
 
-        			$this->_helper->json($response);
+                    $this->_helper->json($response);
 
-        			break;
-        		case 'estaactivado':
-        			//*--- recepcion de datos
-        			$tel = $_POST['tel'];
-        			$codigoactivacion = $_POST['codigoactivacion'];
-        			//*--- envio de datos
-        			$response["success"] = "";
+                    break;
+                case 'estaactivado':
+                    // *--- recepcion de datos
+                    $phone = $_POST['tel'];
+                    $codigoactivacion = $_POST['codigoactivacion'];
+                    // *--- envio de datosverifyActivavionTaxi
+                    $response["success"] = "";
 
-        			$this->_helper->json($response);
+                    $taxiRepository = $this->_entityManager->getRepository(
+                            'Model\Taxi');
+                    if ($taxiRepository->verifyActivavionTaxi($codigoactivacion,
+                            $phone)) {
+                        $response["success"] = "OK";
+                    }
+                    $this->_helper->json($response);
+                    break;
+                case 'envioposicion':
+                    // *--- recepcion de datos
+                    $tel = $_POST['tel'];
+                    $codigoactivacion = $_POST['codigoactivacion'];
+                    $latitud = $_POST['lat'];
+                    $longitud = $_POST['long'];
+                    $timenow = $_POST['tmp'];
+                    $status = $_POST['estado'];
+                    // *--OBTENER MODEL TAXIL
+                    $taxiRepo = $this->_entityManager->getRepository('Model\Taxi');
+                    $taxi = $taxiRepo->findByPhoneCodigoActivacion($tel, $codigoactivacion);
+                    if ($taxi != NULL) {
+                        $taxi->setStatus($status);
 
-        			break;
-        		case 'envioposicion':
-        			//*--- recepcion de datos
-        			$tel = $_POST['tel'];
-        			$codigoactivacion = $_POST['codigoactivacion'];
-        			$lat = $_POST['lat'];
-        			$long = $_POST['long'];
-        			$tmp = $_POST['tmp'];
-        			$estado = $_POST['estado'];
+                        $backtrack = new Backtrack();
+                        $backtrack->setTaxi($taxi)
+                            ->setLatitud($latitud)
+                            ->setLongitud($longitud)
+                            ->setTimenow(new DateTime('now'))
+                            ->setStatus($status)
+                            ->setCreated(new DateTime('now'))
+                            ->setState(TRUE);
 
-        			//*--- envio de datos
-        			$response["success"] = "OK";//vacio en caso de error
-        			$response["peticion"] = "OK"; //si hay una petición para asignar carrera, envía "OK" y esto sólo ocurre cuando el estado enviado por el taxi, sea libre (0). En este momento el taxi deberá cambiar a estado ocupado. En otro caso se envía vacío "". Recordar que la aplicación android debe cambiar de libre a ocupado e, inmediatamente, informar su cambio de estado. Ver el diagráma de estados mostrado arriba.
+                        $this->_entityManager->persist($taxi);
+                        $this->_entityManager->flush();
 
-        			$this->_helper->json($response);
+                        $this->_entityManager->persist($backtrack);
+                        $this->_entityManager->flush();
 
-        			break;
-        		case 'cambioestado':
-        			//*--- recepcion de datos
-        			$tel = $_POST['tel'];
-        			$codigoactivacion = $_POST['codigoactivacion'];
-        			$estado = $_POST['estado'];
+                        // *--- envio de datos
+                        $response["success"] = "OK"; // vacio en caso de error
+                        $response["peticion"] = ""; // si hay una petición para
+                                                      // asignar carrera, envía "OK"
+                                                      // y esto sólo ocurre cuando
+                                                      // el estado enviado por el
+                                                      // taxi, sea libre (0). En
+                                                      // este momento el taxi deberá
+                                                      // cambiar a estado ocupado.
+                                                      // En otro caso se envía vacío
+                                                      // "". Recordar que la
+                                                      // aplicación android debe
+                                                      // cambiar de libre a ocupado
+                                                      // e, inmediatamente, informar
+                                                      // su cambio de estado. Ver el
+                                                      // diagráma de estados
+                                                  // mostrado arriba.
+                    }
 
-        			//*--- envio de datos
-        			$response["success"] = "OK";//vacio en caso de error
-        			$response["peticion"] = "OK"; //si hay una petición para asignar carrera, envía "OK" y esto sólo ocurre cuando el estado enviado por el taxi, sea libre (0). En este momento el taxi deberá cambiar a estado ocupado. En otro caso se envía vacío "". Recordar que la aplicación android debe cambiar de libre a ocupado e, inmediatamente, informar su cambio de estado. Ver el diagráma de estados mostrado arriba.
+                    $this->_helper->json($response);
 
-        			$this->_helper->json($response);
-        			break;
-        	}
+                    break;
+            }
         }
     }
 
     /**
-     * This method generates the key to request password, and returns if it is not duplicate
+     * This method generates the key to request password, and returns if it is
+     * not duplicate
+     *
      * @return string (random key)
      */
-    public function generateKey() {
-    	$sw = TRUE;
-    	do {
-    		$key = substr(str_shuffle(str_repeat('ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789', 4)), 0, 32);
-    		$taxiRepo = $this->_entityManager->getRepository('Model\Taxi');
-    		$taxi = $taxiRepo->findByCodeactivation($key);
+    public function generateKey ()
+    {
+        $sw = TRUE;
+        do {
+            $key = substr(
+                    str_shuffle(
+                            str_repeat(
+                                    'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789',
+                                    4)), 0, 32);
+            $taxiRepo = $this->_entityManager->getRepository('Model\Taxi');
+            $taxi = $taxiRepo->findByCodeactivation($key);
 
             if ($taxi == NULL) {
-    			$sw = FALSE;
-    			break;
-    		}
-    	} while ($sw);
+                $sw = FALSE;
+                break;
+            }
+        } while ($sw);
 
-    	return $key;
+        return $key;
     }
 }
